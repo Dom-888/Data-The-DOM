4e5d7351e8f294f060c253046e6338d0e52ac7af
Dom-888 <spinelladomenico88@gmail.com>
2020-02-04 20:58:40 +0000

    Tabular Data - Part One

--- main.js
+++ main.js
@@ -14,16 +14,29 @@ function getData(type, cb) { //The second parameter stand for callback
 
 }
 
+function getTableHeaders(obj) {//This function take every key of an array and store them inside an empty array
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
 //The following function is called in html
 function writeToDocument(type) { //The argument is passed in line (people, film, starship ecc..)
     var el = document.getElementById("data");
     el.innerHTML = ""; //Every time the function is called (on click), the variable is emptied before is loaded again, this prevent the arrays to stack on top of each others
 
-    getData(type, function(data) { //Data is the JSON object
-        data = data.results; //.results are properties of those specific objects (exept films)
-        
-        data.forEach(function(item) {//Since JSON objects can be treated as arrays, the .forEach can be used to get a list to be "printed" on the page using .innerHTML as setter
+    getData(type, function(data) { //This callback just pass the JSON object, in fact data == cb
+        data = data.results; //.results are properties (arrays) of those specific objects
+        var tableHeaders = getTableHeaders(data[0]); //Store the first key of the results property (data = data.results). It will be used as table header
+
+        data.forEach(function(item) { //Since JSON objects can be treated as arrays, the .forEach can be used to get a list to be "printed" on the page using .innerHTML as setter
             el.innerHTML += "<p>" + item.name + "</p>";
         });
+
+        el.innerHTML =`<table>${tableHeaders}</table>`; 
     });
 };
