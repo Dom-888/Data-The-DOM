977ca3de91b7b53c1684f178f5f299fefabcfad0
Dom-888 <spinelladomenico88@gmail.com>
2020-02-04 17:44:30 +0000

    Finished: Unpacking Our Data Onto The DOM

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 const baseURL = "https://swapi.co/api/";
 
-function getData(type, cb) {
+function getData(type, cb) { //The second parameter stand for callback
     var xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function() {
+    xhr.open("GET", baseURL + type + "/"); //Create an url combining the baseURL and the type, and download it
+    xhr.send();
+
+    xhr.onreadystatechange = function() { //Convert the downloaded string into a js object
         if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
+            cb(JSON.parse(this.responseText)); //The argument for cb is created here, will refer to it as data in the callback function 
         }
     };
 
-    xhr.open("GET", baseURL + type + "/");
-    xhr.send();
 }
 
-function writeToDocument(type) {
-    getData(type, function(data) {
-        document.getElementById("data").innerHTML = data;
+//The following function is called in html
+function writeToDocument(type) { //The argument is passed in line (people, film, starship ecc..)
+    var el = document.getElementById("data");
+    el.innerHTML = ""; //Every time the function is called (on click), the variable is emptied before is loaded again, this prevent the arrays to stack on top of each others
+
+    getData(type, function(data) { //Data is the JSON object
+        data = data.results; //.results are properties of those specific objects (exept films)
+        
+        data.forEach(function(item) {//Since JSON objects can be treated as arrays, the .forEach can be used to get a list to be "printed" on the page using .innerHTML as setter
+            el.innerHTML += "<p>" + item.name + "</p>";
+        });
     });
-}
+};
