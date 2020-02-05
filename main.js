4f57d53517a2617250420014739d9195b1212a62
Dom-888 <spinelladomenico88@gmail.com>
2020-02-05 12:12:14 +0000

    Add pagination

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-const baseURL = "https://swapi.co/api/";
 
-function getData(type, cb) { //The second parameter stand for callback
+function getData(url, cb) { //The second parameter stand for callback
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", baseURL + type + "/"); //Create an url combining the baseURL and the type, and download it
+    xhr.open("GET", url); 
     xhr.send();
 
     xhr.onreadystatechange = function() { //Convert the downloaded string into a js object
@@ -31,22 +30,22 @@ function generatePaginationButtons(next, prev) {
         return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
         return `<button onclick="writeToDocument('${next}')">Next</button>`;
     } else if (next && !prev) {
-        return `<button onclick="writeToDocument('${next}')">Next</button>`;}
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
     } else if (!next && prev) {
-        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;}        
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }        
 }
 
 
 //The following function is called in html
-function writeToDocument(type) { //The argument is passed in line (people, film, starship ecc..)
+function writeToDocument(url) { //The argument is passed in line (people, film, starship ecc..)
     var tableRows = [];
     var el = document.getElementById("data");
    
-    getData(type, function(data) { //This callback just pass the JSON object, in fact data == cb
+    getData(url, function(data) { //This callback just pass the JSON object, in fact data == cb
+        var pagination ="";
         
-        //??
-        if (data.next || data.previous) {
-            var pagination; //Empty var
+        if (data.next || data.previous) { //next and previous are two properties of those specific objects, if at least one exists, the function that creates the buttons is called
             pagination = generatePaginationButtons(data.next, data.previous)
         }
         
@@ -55,6 +54,7 @@ function writeToDocument(type) { //The argument is passed in line (people, film,
 
         data.forEach(function(item) { //Create a row of data for every record in the array and store it in the dataRow var
             var dataRow = [];
+
             Object.keys(item).forEach(function(key) {
                 var rowData = item[key].toString(); 
                 var truncatedData = rowData.substring(0, 15); //Truncates the row to prevent horizontal scrolling
